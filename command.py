fa3f728790758fd9446510296e4ed9f11e4ec7cb
Erica Nazimowitz <nazime@rpi.edu>
2023-04-14 23:54:48 -0400

    Update command.py

--- command.py
+++ command.py
@@ -11,6 +11,18 @@ import commandReceiver
 
 from abc import ABC, abstractmethod #for abstract classes
 
+
+'''
+arguments: 
+    timeStr: a string of time in format hours:minutes
+returns: 
+    the time in hours and minutes
+modifies: 
+    none
+Description:
+    Helper function to convert time from hours:minutes format
+to an integer format.
+'''
 def extractTime(timeStr):
     #expected timeStr format = hours:minutes
     time = timeStr.split(':')
@@ -21,6 +33,20 @@ def extractTime(timeStr):
 
 class Command(ABC):
     cr = commandReceiver.CommandReceiver()
+        '''
+    arguments: 
+        self.bot: The Discord bot itself
+        self.ctx: Command context
+        self.username: The username of the current user using the bot
+        self.args: Command arguments
+        self.cr: The receiver for this class
+    returns: 
+        none
+    modifies: 
+        this
+    Description:
+        Constructor for the Command class.
+    '''
     def __init__(self, bot, ctx, com_rec, args=None):
         self.bot = bot
         self.ctx = ctx #command context 
@@ -28,16 +54,39 @@ class Command(ABC):
         self.userID = int(ctx.author.id)
         self.args = args #command arguments 
         self.cr = com_rec  #receiver for this class
-        
-    #returns true if the author of the message mathces the author of the command 
+     
+    '''
+    arguments: 
+        msg: The last sent message
+    returns: 
+        true if the author of the message matches the author of the command
+        false otherwise 
+    modifies: 
+        none
+    Description:
+        Helper function. Checks the message ID. If the author of the message 
+        matches the author of the command, returns true. Returns false otherwise.
+    '''    
     def checkUser(self, msg):
         if(int(msg.author.id) != self.userID):
             return False
         else:
             return True
 
-    #returns true if the message has the correct time format and the author matches the command author
-    #and false otherwise  
+    '''
+    arguments: 
+        msg: The last sent message
+    returns: 
+        true if the message has the correct time format and the author 
+        matches the command author
+        false otherwise 
+    modifies: 
+        none
+    Description:
+        Helper function. Checks the time given. If the time is in the correct
+        format, returns true and the result of self.checkUser(msg). 
+        Returns false otherwise.
+    '''
     def checkTimeFormat(self, msg):
         time = msg.content.split(':')
 
@@ -51,15 +100,38 @@ class Command(ABC):
             return False
         else:
             return (True and self.checkUser(msg)) 
-
-    #returns true if the message is an integer and the athor matches the author of the command 
-    #and false otherwise
+    '''
+    arguments: 
+        msg: The last sent message
+    returns: 
+        true if the message is an integer and the author matches the author of the command 
+        false otherwise
+    modifies: 
+        none
+    Description:
+        Helper function. Checks the content of the message. If message is a digit,
+        and self.checkUser(msg) returns true, returns true. Returns false otherwise.
+    '''
     def checkIntFormat(self, msg):
         return msg.content.isdigit() and self.checkUser(msg)
 
-	#waits for a response which matches the format as specified by the checkFunc function which
-	#takes a msg and return either true or false depending on whether the message matches the conditions. 
-	#If there is no response in specified time limit in minutes, the fucntion times out. 
+    '''
+    arguments: 
+        checkFunc: A function that returns true if the response meets the criteria the bot
+        is looking for
+        time=5: The maximum number of seconds the bot will wait for until it times out
+    returns: 
+        true if the message matches the condition depending on whether the message matches
+        the given conditions. false otherwise, or an error message if the request times out
+        false otherwise
+    modifies: 
+        none
+    Description:
+        Waits for a response which matches the format as specified by the checkFunc function which
+        takes a message and return either true or false depending on whether the message matches 
+        the conditions. If there is no response in specified time limit in minutes, the
+        function times out. 
+    '''
     async def waitForResponse(self, checkFunc, time=5):
         try:
             msg = await self.bot.wait_for("message", check=checkFunc, timeout= time * 60) 
@@ -67,13 +139,37 @@ class Command(ABC):
             return await self.ctx.send('{}, your request timed out.'.format(self.username)) 
         return msg
 
-    @abstractmethod #execution method responsbile for executing said command 
-    async def execute(self): #variable checker functions
+
+    '''
+    arguments: 
+        none
+    returns: 
+        none
+    modifies: 
+        none
+    Description:
+        Execution method responsible for executing a command.
+    '''
+    @abstractmethod
+    async def execute(self): 
         pass
 
 #-------------------------------------------------------------------------------------------------------------------------------
 
-class NewBacklog(Command):  
+class NewBacklog(Command):
+    '''
+    arguments: 
+        backlogs: The list of all backlogs.
+    returns: 
+        The result of sending an error or successful backlog message.
+    modifies: 
+        backlogs
+    Description:
+        Checks to see if the user already has a backlog. If true,
+        the function ends. Otherwise, prompts the user to enter preferred
+        genres, amount of available playtime, and creates the backlog
+        based on those two variables.
+    '''  
     async def execute(self, backlogs):
         #check to see if the user already has a backlog
         if self.username in backlogs:
@@ -94,6 +190,19 @@ class NewBacklog(Command):
 
 
 class AddGame(Command):
+    '''
+    arguments: 
+        backlog: The given backlog.
+        nameMsg: The current message.
+    returns: 
+        -1 if the game already exists in backlog, and 1 otherwise.
+    modifies: 
+        none
+    Description:
+        Uses a for loop to check over the backlog's contents and
+        compares the name of the game to the message's game name.
+        Returns -1 if the names are the same and 1 otherwise.
+    '''  
     def duplicateCheck(self, backlog, nameMsg):
         for i in range(len(backlog.catalog)):
             current_name = (backlog.catalog[i]).getName()
@@ -103,6 +212,21 @@ class AddGame(Command):
                 return -1
         return 1
 
+    '''
+    arguments: 
+        backlogs: The list of all backlogs.
+    returns: 
+        The result of sending an error message or successful added game.
+    modifies: 
+        backlogs
+    Description:
+        If the user has yet to create a backlog, sends an error message.
+        Otherwise, prompts the user for the game's name, checks to make
+        sure it is not a duplicate, prompts the user for the game's genre,
+        playtime needed to complete the game, time already played of
+        the game, and the current interest in the game. Returns that the
+        game was successfully added to the backlog.
+    '''  
     async def execute(self, backlogs):
         if self.username not in backlogs: 
             return await self.ctx.send("You have yet to create a backlog, {}. Enter /helpBacklog for more info.".format(self.username))
@@ -111,12 +235,9 @@ class AddGame(Command):
             await self.ctx.send("Please enter the game's name.")
             nameMsg = await self.waitForResponse(self.checkUser)
 
-
-
             while self.duplicateCheck(backlogs[self.username], nameMsg) == -1:
                 await self.ctx.send('You cannot add this game to your backlog, {}. There is already a game with the same name in your backlog. Please enter a different name for the game.'.format(self.username))	
                 nameMsg = await self.waitForResponse(self.checkUser)
-
 
             #prompt the user for the the game's genres
             await self.ctx.send("Please enter the game's genre(s) seperated by spaces.")
@@ -138,6 +259,18 @@ class AddGame(Command):
             return await self.ctx.send('Successfully added this new game to your backlog {}.'.format(self.username))	
 
 class DeleteGame(Command):
+    '''
+    arguments: 
+        backlogs: The list of all backlogs.
+    returns: 
+        The result of sending an error message or successful deleted game.
+    modifies: 
+        backlogs
+    Description:
+        If the game to be deleted was not found, returns an error message.
+        Otherwise, removes the game from the backlog and returns that it 
+        was deleted.
+    '''
 	async def execute(self, backlogs):
 		name = self.args[0]
 		if(self.cr.deleteGameRec(backlogs, name, self.username)): #if backlog could not find said game to remove
@@ -147,6 +280,21 @@ class DeleteGame(Command):
 
 #May need to modify function to reduce coupling
 class EditGame(Command):
+    '''
+    arguments: 
+        backlogs: The list of all backlogs.
+    returns: 
+        The result of sending an error message or successful edited game.
+    modifies: 
+        backlogs
+    Description:
+        If the user has yet to create a backlog, sends an error message.
+        If the game does not exist in the backlog, sends an error message.
+        Otherwise, prompts the user for an attribute to edit. If the response
+        is not recognized, sends a message saying so. Otherwise, edits the
+        attribute as the user provides. Returns that the game was successfully
+        edited.
+    '''
     async def execute(self, backlogs):
         if self.username not in backlogs:#checks if backlog exists
             return await self.ctx.send('I am sorry {}, it seems that you have not yet created a backlog, if you would \
@@ -215,6 +363,19 @@ class EditGame(Command):
             
 
 class EditBacklog(Command):
+    '''
+    arguments: 
+        backlogs: The list of all backlogs.
+    returns: 
+        The result of sending an error message or successful edited backlog.
+    modifies: 
+        backlogs
+    Description:
+        If the user has yet to create a backlog, sends an error message.
+        Otherwise, prompts the user to either edit the backlog's preferred
+        genres, or the average amount of available playtime. Returns that the
+        backlog was successfully edited.
+    '''
     async def execute(self, backlogs):
         #NOTE: consider regrading all the backlog games based on the new proposed change
         #basically call "regrade" at the end of this function, no matter which option was selected
@@ -246,13 +407,20 @@ class EditBacklog(Command):
                     await self.ctx.send("If you wish to edit your backlog's preferred genres, enter 1.\nIf you wish to edit your average available playing time, enter 2.")			
                     editMsg = await self.waitForResponse(self.checkUser)		
 
-        await self.ctx.send(self.username + "'s Backlog\n" + '='*15+"\n")  
-        await self.ctx.send("Genre(s): " + str(backlogs[self.username].userGenres))
-        await self.ctx.send("\nAverage Available Time: " + str(backlogs[self.username].avgAvailableTime)  )
-        await self.ctx.send('='*50+"\n")          
         return await self.ctx.send("Backlog Updated!")  
 
 class List(Command):
+    '''
+    arguments: 
+        backlogs: The list of all backlogs.
+    returns: 
+        The result of sending an error message or the list of games in the backlog.
+    modifies: 
+        none
+    Description:
+        If the user has yet to create a backlog, sends an error message.
+        Otherwise, sends a list of the user's games in their backlog.
+    '''
     async def execute(self, backlogs):
         if self.username not in backlogs: 
             return await self.ctx.send("You have yet to create a backlog, {}. Enter /helpBacklog for more info.".format(self.username))
@@ -260,6 +428,18 @@ class List(Command):
             return await self.ctx.send(self.cr.listRec(backlogs, self.username))
 
 class SuggestGames(Command):
+    '''
+    arguments: 
+        backlogs: The list of all backlogs.
+    returns: 
+        The result of sending an error message or a list of games suggested to play.
+    modifies: 
+        none
+    Description:
+        If the user has yet to create a backlog, sends an error message.
+        Otherwise, runs an algorithm to calculate recommended games and
+        returns a list of up to 20 recommended games.
+    '''
     async def execute(self, backlogs):
         #list games in order of their scores, higher scores first  
         if self.username not in backlogs:
@@ -269,6 +449,18 @@ class SuggestGames(Command):
 
 
 class HelpBacklog(Command):
+    '''
+    arguments: 
+        none
+    returns: 
+        A list of all commands for the bot, or a description of the command if
+        an argument is given.
+    modifies: 
+        none
+    Description:
+        Returns a list of all commands, or a detailed description of one command
+        if an argument is specified.
+    '''
     async def execute(self): 
 
         if(self.args == None or len(self.args) == 0):
